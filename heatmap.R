b268fa987c80fe63a1183f0d9d90c3f97383e40d
Adrian Cantu <garbanyo@gmail.com>
2021-07-09 14:41:53 -0400

    added some code to make figures with charles data

--- heatmap.R
+++ heatmap.R
@@ -21,21 +21,6 @@ pwas.time <- function(x){
   res
 }
 
-# tabs <-
-#   lapply(pWAS.list,
-#          function(pt){
-#            output_list = list()
-#            time <- pwas.time(pt$d$timePoint)
-#            table <- table(pt$d$posid,factor(time,unique(time)))>0
-#            table_sum <- crossprod(table)
-#            counts <- data.frame(table) %>% summarise_all(~sum(.)) %>% simplify()
-#            names(counts) <-  colnames(table_sum)
-#            output_list$table  <- table_sum
-#            output_list$counts <- counts
-#            browser()
-#            return(output_list)
-#            })
-
 tabs <-
   imap(pWAS.list,
          function(pt,x){
@@ -54,40 +39,16 @@ tabs <-
 
 # > head(table(pt$d$posid,factor(time,unique(time))))
 # 
-# 0 DAYS 1 MONTHS 1.5 MONTHS 3 MONTHS 6 MONTHS 1 YEARS 1.5 YEARS 2 YEARS 2.5 YEARS 3 YEARS 3.5 YEARS 4 YEARS 4.5 YEARS 5 YEARS
-# chr1_GL383518v1_alt-145344      0        0          0        0        0       1         0       0         0       0         0       0         0       0
-# chr1_GL383518v1_alt-145817      0        0          0        0        1       0         0       0         0       0         0       0         0       0
-# chr1_GL383518v1_alt-146914      0        0          0        0        0       0         0       1         0       2         2       0         0       0
-# chr1_GL383518v1_alt-147164      0        0          1        0        0       0         0       0         0       0         0       0         0       0
-# chr1_GL383518v1_alt-82897       0        0          0        0        0       0         0       0         0       1         2       0         0       0
-# chr1_GL383518v1_alt+133749      0        0          0        0        0       0         0       0         1       0         0       0         0       0
+#                            0 DAYS   1 MONTHS 1.5 MONTHS 3 MONTHS 6 MONTHS 1 YEARS   1.5 YEARS  2 YEARS 2.5 YEARS 3 YEARS 3.5 YEARS  4 YEARS  4.5 YEARS   5 YEARS
+# chr1_GL383518v1_alt-145344      0        0          0         0        0       1         0       0         0       0         0       0         0         0
+# chr1_GL383518v1_alt-145817      0        0          0         0        1       0         0       0         0       0         0       0         0         0
+# chr1_GL383518v1_alt-146914      0        0          0         0        0       0         0       1         0       2         2       0         0         0
+# chr1_GL383518v1_alt-147164      0        0          1         0        0       0         0       0         0       0         0       0         0         0
+# chr1_GL383518v1_alt-82897       0        0          0         0        0       0         0       0         0       1         2       0         0         0
+# chr1_GL383518v1_alt+133749      0        0          0         0        0       0         0       0         1       0         0       0         0         0
 
-
-
-
-#t1 <- tabs$pWAS02$table
-#t1_c <- tabs$pWAS02$counts
-#rownames(t1)
-#as.data.frame(t1) %>% rownames_to_column(var = "rowname") %>% mutate_if(is_numeric,~./sum(.))
-
-# factor_order <- c("0 DAYS","1 MONTHS","1.5 MONTHS","3 MONTHS","6 MONTHS",
-#                   "1 YEARS","1.5 YEARS","2 YEARS","2.5 YEARS","3 YEARS",
-#                   "3.5 YEARS","4 YEARS","4.5 YEARS","6 YEARS")
-# 
-# 
-# t1_df <- as.data.frame(t1) %>%
-#   rownames_to_column(var = "y") %>% 
-# #  mutate(across(where(is.numeric),~./max(.))) %>%
-#   pivot_longer(!y,values_to='val', names_to='x') %>%
-#   mutate(x=fct_relevel(x,factor_order[factor_order %in% x])) %>%
-#   mutate(y=fct_relevel(as.factor(y),factor_order[factor_order %in% y])) %>%
-#   mutate(y_count=t1_c[as.character(y)],x_count=t1_c[as.character(x)]) %>%
-#   mutate(jaccard=(val/(x_count+y_count-val)))
-
-#  fct_relevel(as.factor(t1_df$y),factor_order[factor_order %in% t1_df$y])
 
 long_tables <- imap(tabs,function(tab,name){
-  #browser()
   factor_order <- c("0 DAYS","1 MONTHS","1.5 MONTHS","3 MONTHS","6 MONTHS","9 MONTHS",
                     "1 YEARS","1.5 YEARS","2 YEARS","2.5 YEARS","3 YEARS",
                     "3.5 YEARS","4 YEARS","4.5 YEARS","5 YEARS","6 YEARS")
@@ -128,9 +89,12 @@ date_order_per_date <- date_order_per_date_tmp[date_order_per_date_tmp != 'Na']
 
 names_order_per_date_tmp <- as.vector(outer(long_names,factor_order, FUN = get_first_valid,ss=SS))
 names_order_per_date <- names_order_per_date_tmp[names_order_per_date_tmp != 'Na']
-per_date_sample_color <- sapply(as.integer(as.factor(names_order_per_date)),function(x) {return(hcl.colors(5, "viridis")[x])} )
-
-sample_to_color_df <- data.frame(sample=names(long_tables),color=hcl.colors(5, "viridis"))
+# old viridis text
+#per_date_sample_color <- sapply(as.integer(as.factor(names_order_per_date)),function(x) {return(hcl.colors(5, "viridis")[x])} )
+#sample_to_color_df <- data.frame(sample=names(long_tables),color=hcl.colors(5, "viridis"))
+div_text_color <- c('#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e')
+per_date_sample_color <- sapply(as.integer(as.factor(names_order_per_date)),function(x) {return(div_text_color[x])} )
+sample_to_color_df <- data.frame(sample=names(long_tables),color=div_text_color)
 #========
 
 get_first_valid_inv <- function(x,y,ss) ifelse(is_valid(x,y,ss),x,'Na')
@@ -140,14 +104,17 @@ names_order_per_sample <- names_order_per_sample_tmp[names_order_per_sample_tmp!
 
 date_order_per_sample_tmp <- as.vector(outer(factor_order, long_names, FUN = get_first_valid_inv,ss=SS))
 date_order_per_sample <- date_order_per_sample_tmp[date_order_per_sample_tmp!= 'Na']
-per_sample_sample_color <-sapply(as.integer(as.factor(names_order_per_sample)),function(x) {return(hcl.colors(5, "viridis")[x])} )
+#per_sample_sample_color <-sapply(as.integer(as.factor(names_order_per_sample)),function(x) {return(hcl.colors(5, "viridis")[x])} )
+per_sample_sample_color <-sapply(as.integer(as.factor(names_order_per_sample)),function(x) {return(div_text_color[x])} )
 
 pp <- reduce(long_tables,rbind) %>% 
   mutate(x_s=fct_relevel(x_s,factor_order_per_date)) %>%
   mutate(y_s=fct_relevel(y_s,factor_order_per_date)) %>%
   ggplot( aes(x_s,y_s, fill= jaccard,color=sample_name)) + 
   geom_tile(size=0) +
-  scale_fill_binned(breaks = c(0.001, 0.2, 0.4,0.6),low="white", high="blue",show.limits = TRUE) +
+  scale_fill_stepsn(breaks = c(0.001,0.1, 0.25, 0.5),colors=c('#ffeda0','#feb24c','#f03b20','#000000'),values = c(0.05,0.25,0.5,0.8),show.limits = TRUE,na.value="grey90") +
+  #scale_fill_stepsn(breaks = c(0.001, 0.25, 0.5),colors=c('#ffeda0','#feb24c','#f03b20','#000000'),values = c(0,0.25,0.5,0.8),show.limits = TRUE) +
+  #scale_fill_binned(breaks = c(0.001, 0.25, 0.5),colors=c('#ffeda0','#feb24c','#f03b20','#000000'),values = c(0,0.25,0.5,0.8),show.limits = TRUE) +
   #scale_fill_gradient(low="white", high="black",na.value = 'salmon') +
   #  geom_text(aes(label=round(jaccard,digits = 2)),size=1, color='red') +
   theme_classic() +
@@ -165,7 +132,7 @@ pp <- reduce(long_tables,rbind) %>%
   coord_equal()
 
 
-png(height = 10, width = 10,units = 'in', res=300, file = 'test3.png')
+png(height = 10, width = 10,units = 'in', res=300, file = 'per_date_jaccard.png')
 pp + scale_colour_manual(name = 'the colour', values =c('pWAS02'='black','pWAS03'='red','pWAS04'='black','pWAS05 '='red','pWAS06'='black'),
                          labels =sample_to_color_df$sample ,guide = 'legend') +
   guides(colour=guide_legend("Subject", override.aes=list(shape=22,size=0, fill=sample_to_color_df$color)))
@@ -177,7 +144,8 @@ pp2 <- reduce(long_tables,rbind) %>%
   mutate(y_s=fct_relevel(y_s,factor_order_plus)) %>%
   ggplot( aes(x_s,y_s, fill= jaccard,color=sample_name)) + 
   geom_tile(size=0) +
-  scale_fill_binned(breaks = c(0.001, 0.2, 0.4,0.6),low="white", high="blue",show.limits = TRUE) +
+  scale_fill_stepsn(breaks = c(0.001,0.1, 0.25, 0.5),colors=c('#ffeda0','#feb24c','#f03b20','#000000'),values = c(0.05,0.25,0.5,0.8),show.limits = TRUE,na.value="grey90") +
+  #scale_fill_binned(breaks = c(0.001, 0.2, 0.4,0.6),low="white", high="blue",show.limits = TRUE) +
   #scale_fill_gradient(low="white", high="black",na.value = 'salmon') +
   #  geom_text(aes(label=round(jaccard,digits = 2)),size=1, color='red') +
   theme_classic() +
@@ -194,9 +162,63 @@ pp2 <- reduce(long_tables,rbind) %>%
   #  My_Theme +
   coord_equal()
 
-png(height = 10, width = 10,units = 'in', res=300, file = 'test2.png')
+png(height = 10, width = 10,units = 'in', res=300, file = 'per_subject_jaccard.png')
 pp2 + scale_colour_manual(name = 'the colour', values =c('pWAS02'='black','pWAS03'='red','pWAS04'='black','pWAS05 '='red','pWAS06'='black'),
                         labels =sample_to_color_df$sample ,guide = 'legend') +
   guides(colour=guide_legend("Subject", override.aes=list(shape=22,size=0, fill=sample_to_color_df$color)))
+dev.off()
+
+# charles figures 
+
+ch.final.table <- readRDS('charles_table.rds')
+
+
+factor_order_ch_tmp <- as.vector(outer(factor_order, long_names, FUN =valid_paste,ss=ch.final.table$y_s))
+factor_order_ch <- factor_order_plus_tmp[factor_order_ch_tmp != 'Na']
+
+pp3 <-  ch.final.table %>% mutate(val=val/100) %>%
+  mutate(x_s=fct_relevel(x_s,factor_order_ch)) %>%
+  mutate(y_s=fct_relevel(y_s,factor_order_ch)) %>%
+  ggplot( aes(x_s,y_s, fill=val,color=sample_name)) + 
+  geom_tile(size=0) +
+  #scale_fill_gradientn(breaks = c(0.001,0.1, 0.25, 0.5),colors=c('#ffeda0','#feb24c','#f03b20','#000000'),values = c(0,0.5,0.95,1),na.value="grey90") +
+  scale_fill_viridis_c(direction = -1) +
+    theme_classic() +
+  theme(axis.text.x = element_text(angle = 90),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank()) +
+  #  xlab("Sample") +
+  #  labs(fill='Dilution \nat threshold',
+  #       title = "Quiescent") +
+  #  My_Theme +
+  coord_equal()
+
+png(height = 10, width = 10,units = 'in', res=300, file = 'test4.png')
+pp3 + geom_text(aes(label=round(val,digits = 2)),size=5, color='red') 
+dev.off()
+
+factor_order_ch_date_tmp <- as.vector(outer(long_names,factor_order, FUN = inv_valid_paste, ss=ch.final.table$y_s))
+factor_order_ch_date <- factor_order_ch_date_tmp[factor_order_ch_date_tmp != 'Na']
+
+
+pp4 <-  ch.final.table %>% mutate(val=val/100) %>%
+  mutate(x_s=fct_relevel(x_s,factor_order_ch_date)) %>%
+  mutate(y_s=fct_relevel(y_s,factor_order_ch_date)) %>%
+  ggplot( aes(x_s,y_s, fill=val,color=sample_name)) + 
+  geom_tile(size=0) +
+  #scale_fill_gradientn(breaks = c(0.001,0.1, 0.25, 0.5),colors=c('#ffeda0','#feb24c','#f03b20','#000000'),values = c(0,0.5,0.95,1),na.value="grey90") +
+  scale_fill_viridis_c(direction = -1) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank()) +
+  #  xlab("Sample") +
+  #  labs(fill='Dilution \nat threshold',
+  #       title = "Quiescent") +
+  #  My_Theme +
+  coord_equal()
+
+png(height = 10, width = 10,units = 'in', res=300, file = 'test5.png')
+pp4 + geom_text(aes(label=round(val,digits = 2)),size=5, color='red') 
 dev.off()
 
